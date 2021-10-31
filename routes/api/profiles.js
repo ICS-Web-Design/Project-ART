92f76c9b1327ae0a7b89653d26933b0cd49e7c75
EbenZergaw <eben.zergaw@gmail.com>
2021-10-31 22:09:48 +0300

    Profile route

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -1,6 +1,9 @@
 const { json } = require('express')
+const bcrypt = require('bcryptjs')
 const express = require('express')
 const router = express.Router()
+const jwt = require('jsonwebtoken')
+const config = require('config')
 const {check, validationResult} = require('express-validator')
 const Profile = require('../../models/Profile')
 
@@ -11,9 +14,9 @@ router.post('/', [
     check('firstName', 'First name is required').not().isEmpty(),
     check('lastName', 'Last name is required').not().isEmpty(),
     check("email", "Please include email").isEmail(),
-    check("password", "Enter password").isLength({ min: 6 })
+    check("password", "Enter password").isLength({ min: 6 }),
+    check('grade', 'Grade is required').not().isEmpty(),
 ], async (req, res) => {
-
     const errors = validationResult(req)                        // Checks request for errors
 
     if(!errors.isEmpty()){                                      // Checks if there are errors
@@ -21,35 +24,85 @@ router.post('/', [
         // Returns a bad request message alongside errors array
     }
 
-    const {firstName, lastName, email, password} = req.body
+    const {firstName, lastName, email, password, grade} = req.body
 
     try {
+        
         // See if profile already exists
         let profile = await Profile.findOne({email})
-        if(profile){
-            res.status(400).json([{msg: "User Already Exists"}])
-        }
 
+        if(profile){
+            res.status(400).json({errors: [{msg: "User Already Exists"}]})
+        }
+        
         // Create Profile
         profile = new Profile({
             firstName,
             lastName,
             email,
-            password
+            password,
+            grade
         })
         
         // Encrypt password
         const salt = await bcrypt.genSalt(10);
         profile.password = await bcrypt.hash(password, salt)
-        console.log(profile)
-        await profile.save()
+        await profile.save()                                    // WHY THE FUCK DOESNT THIS WORK?
+        console.log("GREAT SUCCESS")
 
-        // Return JSON Web Token
+        // Return JSON webtoken
+         const payload = {
+            profile: {
+                id: profile.id
+            }
+        }
+
+        jwt.sign(payload, config.get('jwtSecret'), {expiresIn: 36000}, (err, token) => {
+            if(err){
+                throw err
+            } else {
+                res.json({token})
+            }
+        })
     } catch (error) {
-        
+
     }
 
-    res.send(profile)
 })
+
+// @route       GET api/profiles
+// @desc        Get all profile
+// @access      Public
+
+router.get('/', async (req, res) => {
+    let profiles = await Profile.find()
+    res.json(profiles)
+})
+
+// @route       GET api/profiles/:id
+// @desc        Get profile
+// @access      Public
+router.get('/:id', async (req, res) => {
+    
+    try{
+
+        let profile = await Profile.findById(req.params.id)     // Find profile by id
+
+        if(!profile){
+            return res.status(400).json("Profile not found")
+        }
+
+        res.json(profile)
+
+    } catch(err){
+        if(err.kind = "ObjectId"){
+            return res.status(400).json("Profile not found")
+        }
+    }
+
+})
+
+
+
 
 module.exports = router
